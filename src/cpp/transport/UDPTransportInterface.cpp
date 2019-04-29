06071ee8ee64a073ed38dfc350a1fe736316ff9d
Ricardo González Moreno <correoricky@gmail.com>
2019-04-29 14:46:48 +0200

    Refs #5230. Added timeout on UDP.

--- src/cpp/transport/UDPTransportInterface.cpp
+++ src/cpp/transport/UDPTransportInterface.cpp
@@ -16,12 +16,14 @@
 #include <fastrtps/transport/UDPTransportInterface.h>
 #include <fastrtps/rtps/messages/CDRMessage.h>
 #include "UDPSenderResource.hpp"
-#include <utility>
-#include <cstring>
-#include <algorithm>
 #include <fastrtps/log/Log.h>
 #include <fastrtps/utils/Semaphore.h>
 #include <fastrtps/utils/IPLocator.h>
+
+#include <utility>
+#include <cstring>
+#include <algorithm>
+#include <chrono>
 
 using namespace std;
 using namespace asio;
@@ -245,7 +247,6 @@ eProsimaUDPSocket UDPTransportInterface::OpenAndBindUnicastOutputSocket(
     }
     getSocketPtr(socket)->set_option(ip::multicast::hops(configuration()->TTL));
     getSocketPtr(socket)->bind(endpoint);
-    getSocketPtr(socket)->non_blocking(true);
 
     if (port == 0)
     {
@@ -504,7 +505,8 @@ bool UDPTransportInterface::send(
         uint32_t send_buffer_size,
         eProsimaUDPSocket& socket,
         const Locator_t& remote_locator,
-        bool only_multicast_purpose)
+        bool only_multicast_purpose,
+        const std::chrono::microseconds& timeout)
 {
     if (!IsLocatorSupported(remote_locator) || send_buffer_size > configuration()->sendBufferSize)
     {
@@ -522,6 +524,25 @@ bool UDPTransportInterface::send(
 
         try
         {
+#ifndef _WIN32
+            if(timeout.count() > 0)
+            {
+                struct timeval timeStruct;
+                timeStruct.tv_sec = 0;
+                timeStruct.tv_usec = timeout.count();
+                setsockopt(getSocketPtr(socket)->native_handle(), SOL_SOCKET, SO_SNDTIMEO,
+                        reinterpret_cast<const char*>(&timeStruct), sizeof(timeStruct));
+            }
+            else
+            {
+                struct timeval timeStruct;
+                timeStruct.tv_sec = 0;
+                timeStruct.tv_usec = 0;
+                setsockopt(getSocketPtr(socket)->native_handle(), SOL_SOCKET, SO_SNDTIMEO,
+                        reinterpret_cast<const char*>(&timeStruct), sizeof(timeStruct));
+            }
+#endif
+
             asio::error_code ec;
             bytesSent = getSocketPtr(socket)->send_to(asio::buffer(send_buffer, send_buffer_size), destinationEndpoint, 0, ec);
             if(!!ec)
